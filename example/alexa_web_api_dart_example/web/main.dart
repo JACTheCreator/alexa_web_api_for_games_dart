8f36663761fa45335d21370411970978032a065d
JACTheCreator <jermainecoates@outlook.com>
2022-02-23 21:57:41 -0500

    Added performance overlay monitor ui

--- example/alexa_web_api_dart_example/web/main.dart
+++ example/alexa_web_api_dart_example/web/main.dart
@@ -1,5 +1,48 @@
+import 'dart:async';
 import 'dart:html';
 
-void main() {
-  querySelector('#output')?.text = 'Your Dart app is running.';
+import 'package:alexa_web_api/alexa_web_api.dart';
+
+Element? status = querySelector('#status');
+
+Future<void> main() async {
+  try {
+    AlexaReadyPayload payload = await Alexa.create(
+      CreateClientOptions(version: '1.1'),
+    );
+
+    loadPerformaceMonitor(payload.alexa);
+  } on ErrorWithCode catch (e) {
+    status?.text = 'Status: ${e.message} | ${e.code}';
+  } catch (e) {
+    status?.text = 'Status: $e';
+  }
+}
+
+void loadPerformaceMonitor(Client alexa) {
+  var availableMemory = querySelector('#available-memory');
+  var supportsPushtoTalk = querySelector('#push-to-talk');
+  var supportsWakeWord = querySelector('#wake-word');
+  querySelectorAll('.ending').forEach((element) {
+    element.style.display = "inline";
+  });
+
+  status?.text = 'Status: Alexa is Ready :D';
+
+  promiseToStream<MemoryInfo, MemoryInfoError>(
+    alexa.performance.getMemoryInfo,
+  ).listen((event) {
+    availableMemory?.text = '${event.availableMemoryInMB}';
+  }).onError((error) {
+    if (error is MemoryInfoError) {
+      status?.text = 'Status: ${error.message}';
+    } else {
+      status?.text = 'Status: $error}';
+    }
+  });
+
+  var microphone = alexa.capabilities.microphone;
+  supportsPushtoTalk?.text =
+      microphone.supportsPushToTalk.toString().toUpperCase();
+  supportsWakeWord?.text = microphone.supportsWakeWord.toString().toUpperCase();
 }
