9547d1fee549544eba9dedc8c6a90033cbc1de03
JACTheCreator <jermainecoates@outlook.com>
2022-03-01 20:53:56 -0500

    Moved the example

--- alexa_web_api_for_games/example/web/main.dart
+++ alexa_web_api_for_games/example/web/main.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'dart:html';
+import 'dart:js';
 
 import 'package:alexa_web_api/alexa_web_api.dart';
 
@@ -7,8 +8,15 @@ Element? status = querySelector('#status');
 
 Future<void> main() async {
   try {
+    final dmp = DefaultMessageProvider();
+    dmp.receive = allowInterop((message) {
+      print(message);
+    });
     AlexaReadyPayload payload = await Alexa.create(
-      CreateClientOptions(version: '1.1'),
+      CreateClientOptions(
+        version: '1.1',
+        messageProvider: dmp,
+      ),
     );
 
     loadPerformaceMonitor(payload.alexa);
@@ -45,4 +53,27 @@ void loadPerformaceMonitor(Client alexa) {
   supportsPushtoTalk?.text =
       microphone.supportsPushToTalk.toString().toUpperCase();
   supportsWakeWord?.text = microphone.supportsWakeWord.toString().toUpperCase();
+}
+
+Stream<T> promiseToStream<T, E extends Object>(dynamic jsPromise,
+    [Duration? interval]) async* {
+  interval = interval ?? Duration(seconds: 1);
+
+  while (true) {
+    T? value;
+    E? error;
+    await promiseToFuture(jsPromise()).then((jsValue) {
+      value = jsValue;
+    }).onError(
+      (E? err, stackTrace) {
+        error = err;
+      },
+    );
+    if (error != null) {
+      yield* Stream<T>.error(error!.toString());
+    } else {
+      yield value!;
+    }
+    await Future.delayed(interval);
+  }
 }
